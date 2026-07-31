7c2c3a334f8af88f2fc27a87cb454330fdb94eba
Aurora-k-412 <3075574856@qq.com>
2026-08-01 01:34:29 +0800

    day04_train_01

--- code/day04_file.py
+++ code/day04_file.py
@@ -1,13 +1,36 @@
-# Day04 Python 文件操作
+print("Day04 Python File")
 
 
-with open("test.txt","w") as f:
-    f.write("AI Internship\n")
-    f.write("Python Learning\n")
+with open("learning.txt","w") as f:
+    f.write("Python File Learning\n")
+    f.write("AI Study 2026\n")
 
 
-with open("test.txt","r") as f:
+with open("learning.txt","r") as f:
     content = f.read()
 
 
 print(content)
+
+
+
+# Day04 小练习1：保存学生信息
+
+students = [
+    "凯凯,20,95\n",
+    "小明,21,88\n",
+    "小红,19,92\n"
+]
+#写入文件
+with open("students.txt","w") as f:
+    for student in students:
+        f.write(student)
+
+#读取文件
+with open("students.txt","r") as f:
+    content = f.read()
+
+print("学生信息：\n" + content)
+
+
+
