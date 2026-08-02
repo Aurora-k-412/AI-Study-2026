b54b754455a36188bb97bcba68bf6babcfb62497
Aurora-k-412 <3075574856@qq.com>
2026-08-02 23:27:33 +0800

    Day05 learn Python module

---
+++ code/day05_module/main.py
@@ -0,0 +1,15 @@
+import calculator
+
+
+result1 = calculator.add(10, 5)
+
+result2 = calculator.subtract(10, 5)
+
+result3 = calculator.multiply(10, 5)
+
+result4 = calculator.divide(10, 2)
+
+print(result1)  # Output: 15
+print(result2)  # Output: 5
+print(result3)  # Output: 50
+print(result4)  # Output: 5.0
---
+++ notes/Day05.md.md
@@ -0,0 +1,21 @@
+# Day05 Python Module 模块
+
+
+## 1. 什么是模块
+
+Python模块就是一个.py文件。
+
+一个文件里面写好的函数，可以被其他Python文件导入使用。
+
+
+例如：
+
+calculator.py
+
+里面定义：
+
+```python
+def add(a,b):
+    return a+b
+![[Pasted image 20260802232146.png]]
+![[Pasted image 20260802232351.png]]
